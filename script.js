78730f52811119f268caa93b04603b5554d492fa
drei <93849500+drei-odin@users.noreply.github.com>
2023-04-10 19:47:42 +0800

    Add code to play five rounds and update score

--- script.js
+++ script.js
@@ -7,31 +7,57 @@ function getComputerChoice() {
     return CHOICES[randNum()];
 }
 
+    let playerScore =  0;
+    let computerScore = 0;
+
 function playGame(playerSelection, computerSelection) {
     let playerPrompt = prompt('Please enter rock, paper, or scissors');
     playerSelection = playerPrompt.toLowerCase();
     computerSelection = getComputerChoice().toLowerCase();
 
     //* TO CHECK SELECTIONS
-    console.log(playerSelection, computerSelection);
+    // console.log(playerSelection, computerSelection);
 
     if (playerSelection === 'paper' && computerSelection === 'rock') {
-        console.log('You win! Paper beats rock!')
+        console.log('You win! Paper beats rock!');
+     playerScore += 1;
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        console.log('You win! Rock beats scissors!')
+        console.log('You win! Rock beats scissors!');
+     playerScore += 1;
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        console.log('You win! Scissors beats paper!')
+        console.log('You win! Scissors beats paper!');
+     playerScore += 1;
     } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-        console.log('You lose! Paper beats rock!')
+        console.log('You lose! Paper beats rock!');
+        computerScore += 1;
     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        console.log('You lose! Scissors beats Paper!')
+        console.log('You lose! Scissors beats Paper!');
+        computerScore += 1;
     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        console.log('You lose! Rock beats scissors!')
+        console.log('You lose! Rock beats scissors!');
+        computerScore += 1;
     } else {
-        console.log(`It's a tie!`)
+        console.log(`It's a tie!`);
     }
 } 
+// Play 5 rounds then declare a winner
+function game() {
+    let rounds = 1;
+    for (let i = rounds; i <= 5; i++) {
+        playGame();
+        console.log(`Your score: ${playerScore} Computer score: ${computerScore}`);
+        if (i === 5) {
+            if (playerScore > computerScore) {
+                console.log(`You win 🏆`);
+            } else if (computerScore > playerScore) {
+                console.log(`Computer wins 🏆`);
+            } else {
+                console.log(`It's a tie 🤷‍♀️`);
+            }
+        }
+    }
+}
 
-playGame();
+game();
 
 // TODO Make scoring system
